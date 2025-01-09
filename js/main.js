5c9e6b19b988ef6c887b922355df10f67b7db357
KChuene <pk1110@yahoo.com>
2025-01-09 12:51:59 +0200

    Progress: Popup disappear, Listview

--- js/main.js
+++ js/main.js
@@ -1,11 +1,39 @@
 document.querySelector("#add-btn").addEventListener(
-    "click", function() {
+    "click", (event) => {
+        event.stopPropagation();
         document.querySelector(".popup").classList.add("active");
     }
 );
 
-document.querySelector("#.popup .close-btn").addEventListener(
-    "click", function() {
+document.querySelector(".popup .close-btn").addEventListener(
+    "click", () => {
         document.querySelector(".popup").classList.remove("active");
     }
 );
+
+document.querySelector("#search-btn").addEventListener(
+    "click", listview
+)
+
+window.addEventListener(
+    "click", (event) => {
+        popup = document.querySelector(".popup");
+
+        if(!popup.contains(event.target)) {
+            popup.classList.remove("active");
+        }
+    }
+);
+
+function listview() {
+    const row = document.querySelector(".row"); 
+
+    items = ["banana", "apple", "mango"];
+    items.forEach(elem => {
+        const column = document.createElement("div");
+        column.classList.add("column");
+        column.textContent = elem;
+        
+        row.appendChild(column);
+    });
+}
